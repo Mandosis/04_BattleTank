94a06d02bb9af5a2a50f2e7877b00aacb2c121f3
Chris Rabuse <rab233@live.com>
2016-11-30 21:47:06 -0600

    Add TankAIController

--- Source/BattleTank/Private/TankPlayerController.cpp
+++ Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,6 +4,24 @@
 #include "Public/TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing: %s"), *ControlledTank->GetName());
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
