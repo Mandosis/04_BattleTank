7f0cab360a319fa1e70e41e7cc43e327be70eba4
Chris Rabuse <rab233@live.com>
2016-11-30 22:21:11 -0600

    Add GetPlayerTank to AIController

--- Source/BattleTank/Private/TankAIController.cpp
+++ Source/BattleTank/Private/TankAIController.cpp
@@ -9,7 +9,6 @@ void ATankAIController::BeginPlay()
 	Super::BeginPlay();
 
 	auto ControlledTank = GetControlledTank();
-
 	if (!ControlledTank)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AIController not possessing a tank"));
@@ -18,9 +17,26 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AIController posessing: %s"), *ControlledTank->GetName());
 	}
+
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController could not find player tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player tank: %s"), *PlayerTank->GetName());
+	}
 }
 
 ATank* ATankAIController::GetControlledTank()
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank()
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
 }
